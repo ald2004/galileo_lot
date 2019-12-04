7a863d6e3f32ebe976e11f7b2714844b286212c7
ald2004 <yuanpu@139.com>
2019-12-04 10:45:33 +0800

    Update plot_data.py

--- detect_formaldehyde/plot_data.py
+++ detect_formaldehyde/plot_data.py
@@ -1,24 +1,30 @@
 # -*- coding: UTF-8 -*-
-#!/usr/bin/python
+# !/usr/bin/python
 
 import json
-# time, os, sys
-# import pandas as pd
-# import seaborn as sns
+import datetime
+import pandas as pd
 import matplotlib.pylab as plt
+import matplotlib.dates as md
+# import matplotlib.ticker as mticker
+
+from matplotlib.pyplot import MultipleLocator
 from PIL import Image
+
+rootdir = ''
+width = 2
 
 if __name__ == '__main__':
     time, datadetail = [], []
-    truetime = ''
-    with open(f'test_0_json.json', 'r') as fp:
-        jsondata = json.load(fp)
-        jsondata = jsondata["Data"]["ResultList"]["ResultList"]
-        truetime = jsondata[0]['time'].split(' ')[0]
+    time_now = datetime.datetime.now()
+    truetime = f'{time_now.year}-{time_now.month:0{width}d}-{time_now.day:0{width}d}'
+
     for i in range(90):
-        with open(f'test_{i}_json.json', 'r') as fp:
+        with open(f'{rootdir}test_{i}_json.json', 'r') as fp:
             jsondata = json.load(fp)
+            if not jsondata['Success']: continue
             jsondata = jsondata["Data"]["ResultList"]["ResultList"]
+
             if len(jsondata) > 0:
                 '''
                 {'time': '2019-12-01 10:01:35', 'data': 'CH4:0.04106|CH2O:0.0000 mg/m3'}, {'time': '2019-12-01 14:27:29', 'data': 'CH4:0.03715|CH2O:0.0000 mg/m3'}, 
@@ -35,17 +41,32 @@ if __name__ == '__main__':
 
                 # print(f'time is :{time}, datadetail is :{datadetail}')
     print(f'total data size is :{len(datadetail)}, and len(time) is {len(time)}')
-    print(time)
+    maxdata = max(datadetail)
+    print(maxdata)
     # print(datadetail)
     # times = pd.date_range(start='2019-12-01',end='2019-12-02',periods=len(time)/3600)
-    for i in range(len(time)):
-        if i % 3455:
-            time[i] = ''
 
-    plt.plot(time, datadetail)
-    plt.savefig(f'get_data_{truetime}.png')
+    df = pd.DataFrame({'toronto_time': time, 'description': datadetail})
+    df['toronto_time'] = pd.to_datetime(df['toronto_time'], format='%H:%M:%S')
+    fig, ax = plt.subplots(figsize=(8, 6))
+    plt.plot('toronto_time', 'description', data=df)
+    ax.set_xlim(df['toronto_time'].min() - pd.Timedelta(1, 'h'),
+                df['toronto_time'].max() + pd.Timedelta(1, 'h'))
+    ax.xaxis.set_major_locator(md.HourLocator(interval=1))
+    ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
+    # ax.xaxis.set_major_locator(mticker.MultipleLocator())
+    y_major_locator = MultipleLocator(3)
+    ax.yaxis.set_major_locator(y_major_locator)
+    plt.tick_params(axis='y', which='major', labelsize=12)
+    fig.autofmt_xdate()
+    ax.set_xlabel('time')
+    ax.set_ylabel('mg/m^3')
+    ax.set_title('formaldehyde')
     # plt.show()
-    image = Image.open(f'get_data_{truetime}.png')
-    # image=image.resize((128,64))
-    gay=image.convert("1")
-    gay.save(f'get_data_{truetime}.png')
+    # plt.plot(time, datadetail)
+    plt.savefig(f'{rootdir}get_data_{truetime}.png')
+    # plt.show()
+    image = Image.open(f'{rootdir}get_data_{truetime}.png')
+    # image = image.resize((128, 64))
+    gay = image.convert("1")
+    gay.save(f'{rootdir}get_data_{truetime}.png')
