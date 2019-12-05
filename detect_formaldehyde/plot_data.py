cd89b65b5ce38f79da589ad17f875dcf2b33a8f9
ald2004 <yuanpu@139.com>
2019-12-05 10:05:10 +0800

    Update plot_data.py

--- detect_formaldehyde/plot_data.py
+++ detect_formaldehyde/plot_data.py
@@ -11,7 +11,8 @@ import matplotlib.dates as md
 from matplotlib.pyplot import MultipleLocator
 from PIL import Image
 
-rootdir = ''
+datadir = '/tmp/'
+savedir = '/root/'
 width = 2
 
 if __name__ == '__main__':
@@ -20,26 +21,28 @@ if __name__ == '__main__':
     truetime = f'{time_now.year}-{time_now.month:0{width}d}-{time_now.day:0{width}d}'
 
     for i in range(90):
-        with open(f'{rootdir}test_{i}_json.json', 'r') as fp:
-            jsondata = json.load(fp)
-            if not jsondata['Success']: continue
-            jsondata = jsondata["Data"]["ResultList"]["ResultList"]
+        try:
+            with open(f'{datadir}test_{i}_json.json', 'r') as fp:
+                jsondata = json.load(fp)
+                if not jsondata['Success']: continue
+                jsondata = jsondata["Data"]["ResultList"]["ResultList"]
 
-            if len(jsondata) > 0:
-                '''
-                {'time': '2019-12-01 10:01:35', 'data': 'CH4:0.04106|CH2O:0.0000 mg/m3'}, {'time': '2019-12-01 14:27:29', 'data': 'CH4:0.03715|CH2O:0.0000 mg/m3'}, 
-                {'time': '2019-12-01 01:01:31', 'data': 'CH4:0.04008|CH2O:0.0000 mg/m3'}
-                '''
-                for xx in jsondata:
-                    try:
-                        a = xx['time'].split(' ')[1]
-                        b = xx['data'].split('|')[1].split(' ')[0].split(':')[1]
-                        time.append(a)
-                        datadetail.append(b)
-                    except:
-                        continue
-
-                # print(f'time is :{time}, datadetail is :{datadetail}')
+                if len(jsondata) > 0:
+                    '''
+                    {'time': '2019-12-01 10:01:35', 'data': 'CH4:0.04106|CH2O:0.0000 mg/m3'}, {'time': '2019-12-01 14:27:29', 'data': 'CH4:0.03715|CH2O:0.0000 mg/m3'}, 
+                    {'time': '2019-12-01 01:01:31', 'data': 'CH4:0.04008|CH2O:0.0000 mg/m3'}
+                    '''
+                    for xx in jsondata:
+                        try:
+                            a = xx['time'].split(' ')[1]
+                            b = xx['data'].split('|')[1].split(' ')[0].split(':')[1]
+                            time.append(a)
+                            datadetail.append(float(b))
+                        except:
+                            continue
+        except FileNotFoundError:
+            continue
+            # print(f'time is :{time}, datadetail is :{datadetail}')
     print(f'total data size is :{len(datadetail)}, and len(time) is {len(time)}')
     maxdata = max(datadetail)
     print(maxdata)
@@ -55,18 +58,16 @@ if __name__ == '__main__':
     ax.xaxis.set_major_locator(md.HourLocator(interval=1))
     ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M:%S'))
     # ax.xaxis.set_major_locator(mticker.MultipleLocator())
-    y_major_locator = MultipleLocator(3)
-    ax.yaxis.set_major_locator(y_major_locator)
-    plt.tick_params(axis='y', which='major', labelsize=12)
+    # y_major_locator = MultipleLocator()
+    # ax.yaxis.set_major_locator(y_major_locator)
+    # plt.tick_params(axis='y', which='major', labelsize=12)
     fig.autofmt_xdate()
     ax.set_xlabel('time')
     ax.set_ylabel('mg/m^3')
     ax.set_title('formaldehyde')
     # plt.show()
-    # plt.plot(time, datadetail)
-    plt.savefig(f'{rootdir}get_data_{truetime}.png')
-    # plt.show()
-    image = Image.open(f'{rootdir}get_data_{truetime}.png')
+    plt.savefig(f'{savedir}get_data_{truetime}.png')
+    image = Image.open(f'{savedir}get_data_{truetime}.png')
     # image = image.resize((128, 64))
     gay = image.convert("1")
-    gay.save(f'{rootdir}get_data_{truetime}.png')
+    gay.save(f'{savedir}get_data_{truetime}.png')
